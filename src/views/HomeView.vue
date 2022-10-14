9696fe436cc08c5ae673a68a069a837fbaab506b
TheNevilleLevel <keeganmollentzemain@gmail.com>
2022-10-14 16:20:27 +0200

    Updated Home folder
    Updated the home folder to show chosen relative show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    Home
+    <div class="feature-card">
+    <router-link to="/movie/tt0409591">
+<img src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+alt="Naruto Poster" class="featured-img" />
+<div class="detail">
+  <h3>Naruto</h3>
+  <p>Naruto Uzumaki, a mischievious adolescent ninja, struggles as he searches for revognition and dreams of becoming Hokage, the village's leader and strongest ninja.
+  </p>
+</div>
+    </router-link>
+  </div>
   </div>
 </template>
 
@@ -10,3 +20,40 @@ export default {
  
 }
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #FFF;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #FFF;
+      }
+    }
+  }
+}
+</style>
